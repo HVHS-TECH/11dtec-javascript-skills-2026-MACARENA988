5fd2dee89ca498f4a57021900f1059affe278f4a
Jacob Williams <24092jw@hvhs.school.nz>
2026-04-28 10:59:59 +1200

    t03_maths.js is complete

--- t02_variables.js
+++ t02_variables.js
@@ -6,15 +6,27 @@ console.log("Running t02_variable.js")
 console.log("/*This is annoying*/")
 
 //Varibles
-var userMoney;
-var userMoney;
-var userName;
+let userMoney;
+let userAge;
+let userName;
+let currentYear;
+
+userName = "Lukys";
+userAge = 15;
+userMoney = 100;
+currentYear = 2026;
+
 /****************************
 Main code:
 ****************************/
 
+console.log("Hi " + userName)
 
+console.log("As of "+ currentYear + " you are " + userAge + " years old")
+
+console.log("You have " + userMoney + " dollars")
 
 /****************************
 Functions:
 ****************************/
+
--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,37 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task:
+****************************/
+
+console.log("Running t03_maths.js")
+console.log("/*This is annoying*/")
+
+//Varibles
+let userMoney;
+let userAge;
+let userName;
+let currentYear;
+
+userName = "Lukys";
+userAge = 15;
+userMoney = 100;
+currentYear = 2026;
+
+/****************************
+Main code:
+****************************/
+
+console.log("Hi " + userName)
+
+console.log("As of "+ currentYear + " you are " + userAge + " years old")
+
+console.log("You were born in ", currentYear - userAge)
+
+console.log("You have " + userMoney + " dollars")
+
+console.log("You spend half of your money, now you have ", userMoney /2)
+
+console.log("Then you get $3, now you have ", userMoney/2 + 3)
+
+/****************************
+Functions:
+****************************/
