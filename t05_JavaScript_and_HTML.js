750496a1e3fb323792eee4da6484a75cc6e42b25
Jacob Williams <24092jw@hvhs.school.nz>
2026-05-04 09:55:58 +1200

    locing in

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -10,11 +10,16 @@ let userMoney;
 let userAge;
 let userName;
 let currentYear;
+let plusThree;
+let divideTwo;
 
 userName = "Lukys";
 userAge = 15;
 userMoney = 100;
 currentYear = 2026;
+divideTwo = userMoney/2;
+plusThree = userMoney/2+3;
+
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 /****************************
@@ -23,12 +28,10 @@ Main code:
 OUTPUT.innerHTML = "<p>Hi " + userName;
 OUTPUT.innerHTML +="<p>As of "+ currentYear + " you are " + userAge + " years old";
 let yearBorn = currentYear - userAge; 
-OUTPUT.innerHTML += "<p>You were born in " + yearBorn;
+OUTPUT.innerHTML += "<p>You were born in " + yearBorn ;
 OUTPUT.innerHTML +="<p>You have " + userMoney + " dollars";
-let userMoney = userMoney / 2;
-OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + userMoney;
-let userMoney = userMoney + 3;
-OUTPUT.innerHTML += "<p>Then you get $3, now you have " + userMoney;
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + divideTwo;
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + plusThree;
 
 /****************************
 Functions:
